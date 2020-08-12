0e4ef415574c1bb2791005946d45d98062ce53ab
Venicyus <venicyusti@gmail.com>
2020-08-11 21:39:42 -0300

    Updates

---
+++ precache-manifest.9bb8a18e7b02ef41452b21e529affc64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da373abea48c1a655fb134ba37209943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aa34085b8c4a28d7d64",
+    "url": "/static/js/2.eabbee64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eabbee64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada1ffc61955d3676ab1",
+    "url": "/static/js/main.31a011d5.chunk.js"
+  },
+  {
+    "revision": "6f842d89ee2e601d78d2",
+    "url": "/static/js/runtime-main.cd30be7a.js"
+  }
+]);
